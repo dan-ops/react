1458a53a07765dd99892291556a09fd9245d9878
dan-ops <sec.daniel24@gmail.com>
2017-01-17 21:35:47 -0600

    with redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>React App</p>
-      </div>
-    );
-  }
-}
+// class Validate extends Component {
+//   constructor(props){
+//     super(props)
+//   }
+//   render(){
+//     const temp = this.props.cel
 
-export default App;
+//     return (
+//       <p>The water will {temp < 100 ? 'not' : ' '}boil!</p>
+//     )
+//   }
+// }
+
+// class Temperatue extends Component {
+
+//   calculateTemp(){
+
+//   }
+
+// }
+
+// class Calculator extends Component {
+//   constructor(props){
+//     super(props)
+
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <Temperature value={this.state.cel} type='c' />
+//         <Temperature value={this.state.fer} type='f' />
+//         <Validate value={this.state.cel} />
+//       </div>
+//     )
+//   }
+
+// }
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+//import React, { Component } from 'react'
+//import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+//todo constants
+const TODO = 'TODO'
+const TOGGLE = 'TOGGLE'
+
+//add todo actionCreator
+function addTodo(text){
+	return { type : TODO, text }
+}
+
+//toggle todo actionCreator
+function toggleTodo(index){
+	return { type : TOGGLE, index }
+}
+
+//todo reducer
+function todos(state = [], action){
+	switch(action.type){
+		case TODO:
+			return [...state, {
+				text : action.text,
+				completed : false
+			}]
+		case TOGGLE:
+			return state.map((todo,index)=>{
+				if(index === action.index){
+					return Object.assign({}, state, {
+						completed : !todo.completed
+					})
+				}
+				return todo
+			})
+		default:
+			return state
+	}
+}
+
+let store = createStore(todos)
+
+let unsubscribe = store.subscribe(()=>
+ 	console.log(store.getState())
+)
+
+store.dispatch(addTodo('keep learning'))
+store.dispatch(addTodo('keep at it'))
+store.dispatch(toggleTodo(1))
+
+unsubscribe()
